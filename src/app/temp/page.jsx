e513f0b6ff8fc0a393728489844ad3f0527e144c
Dasun Thamash <tunathamash@gmail.com>
2026-08-12 12:28:25 +0530

    content updated

---
+++ src/app/temp/page.jsx
@@ -0,0 +1,9 @@
+export default function Temp() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-center text-7xl font-bold">
+        aussaganna epa apith Kariyo thamai mahaththayo 🫵
+      </h1>
+    </div>
+  );
+}
